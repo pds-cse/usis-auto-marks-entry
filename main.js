ad6dc944d67b168ad58c562b981e32acb0616280
pds-cse <purbayandas121@gmail.com>
2025-03-18 02:48:17 +0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+// paste after this line the cell copied from the gradesheet
+
+
+var x = document.getElementsByTagName('td');
+var student_ids_usis = [];
+var m = 28;
+
+// var k = 31;
+// for (let i = 0; i < Math.floor(x.length/13)-1; i++) {
+//     x[k].textContent = '';
+//     k += 13;
+// }
+
+
+//// Comment the following block and uncomment the above one to clear marks for all
+
+var k = 31;
+for (let i = 0; i < Math.floor(x.length/13)-1; i++) {
+    student_ids_usis.push(x[m].textContent);
+    for (let j = 0; j < student_ids_with_grades.length; j += 2) {
+        if(student_ids_usis[i] == student_ids_with_grades[j]) {
+            x[k].textContent = student_ids_with_grades[j+1];
+            break;
+        }
+    }
+    k += 13;
+    m += 13;
+}
